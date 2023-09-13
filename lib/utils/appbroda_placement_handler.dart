26aba455a49ec87bb3864c42fb3ff056e7c56f53
Anil Kumar MR <107169398+anil-mr@users.noreply.github.com>
2023-09-13 17:57:28 +0530

    added additional null checks

--- lib/utils/appbroda_placement_handler.dart
+++ lib/utils/appbroda_placement_handler.dart
@@ -2,37 +2,30 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 class AppBrodaPlacementHandler {
   static void initRemoteConfigAndSavePlacements() async {
-    var firebaseRemoteConfig = FirebaseRemoteConfig.instance;
+    final firebaseRemoteConfig = FirebaseRemoteConfig.instance;
     await firebaseRemoteConfig.setConfigSettings(RemoteConfigSettings(
       // change these settings as per your requirement
       fetchTimeout: const Duration(seconds: 10),
       minimumFetchInterval: const Duration(seconds: 0),
     ));
-
-    fetchAndSavePlacements(firebaseRemoteConfig);
+    await firebaseRemoteConfig.fetchAndActivate();
   }
 
-  static void  fetchAndSavePlacements(FirebaseRemoteConfig remoteConfig) async {
-    await remoteConfig.fetchAndActivate().then((isUpdated) {
-      print('Fetch and activate succeeded!!');
-      if (isUpdated) {
-        print('Updated remote config data found!');
-      } else {
-        print('Remote config data is the same!');
-      }
-    });
+  static void fetchAndSavePlacements() async {
+    await FirebaseRemoteConfig.instance.fetchAndActivate();
   }
 
   static List<String> loadPlacement(String key) {
     String? value = FirebaseRemoteConfig.instance.getString(key);
-    if (value == '') return [];
+    if (value.isEmpty) return [];
 
-    var placement = convertToArray(value);
-    return placement ?? [];
+    return _convertToArray(value);
   }
 
-  static List<String>? convertToArray(String value) {
-    List<String>? array;
+   static List<String> _convertToArray(String value) {
+    List<String> array = [];
+    if(value.isEmpty || value == null) return array;
+
     value = value.substring(1, value.length - 1);
     array = value.split(',');
     for (int i = 0; i < array.length; i++) {
